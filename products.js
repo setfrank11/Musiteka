987abf083e2c73f3cd45731e4db178f840f86d0d
setfrank11 <105731465+setfrank11@users.noreply.github.com>
2023-05-21 21:38:34 -0600

    Add files via upload

---
+++ products.js
@@ -0,0 +1,77 @@
+const productos = [
+    {
+        id:1,
+        productName: "Kissme - The Cure",
+        price: 210,
+        quanty: 1,
+        img: "./imagenes/kissme.jpg",
+    },
+    {
+        id: 2,
+        productName: "Memo Rex Commander - Zoe",
+        price: 180,
+        quanty:1,
+        img: "./imagenes/Memo Rex Commander y el Corazón Atómico de la Vía Láctea [2.jpg",
+   },
+   {
+    id: 3,
+    productName: "Raindows - Radiohead",
+    price: 195,
+    quanty: 1,
+    img: "./imagenes/homepage_large.7055fb4d.jpg"
+   },
+   {id: 4,
+    productName: "Violator - Depeche Mode",
+    price: 195,
+    quanty: 1,
+    img: "./imagenes/violator1_.jpg"
+   },
+   {id: 5,
+    productName: "Alps - Motorama",
+    price: 185,
+    quanty: 1,
+    img: "./imagenes/motorama-alp.jpg"
+   },
+   {id: 6,
+    productName: "Use your illusion - Guns and Rose",
+    price: 195,
+    quanty: 1,
+    img: "./imagenes/guns-use-your-illusion-i.jpg"
+   },
+   {id: 7,
+    productName: "Ok computer - Radiohead",
+    price: 215,
+    quanty: 1,
+    img: "./imagenes/okcomputer.jpg"
+   },
+   {id: 8,
+    productName: "El nervio del volcan - Caifanes",
+    price: 215,
+    quanty: 1,
+    img: "./imagenes/el nerviodelvolcan.webp"
+   },
+   {id: 9,
+    productName: "Overpowered - Roisin Murphy",
+    price: 205,
+    quanty: 1,
+    img: "./imagenes/roisyn - over.jpg"
+   },
+   {id: 10,
+    productName: "4 caminos - Cafe Tacuba",
+    price: 255,
+    quanty: 1,
+    img: "./imagenes/cafe-cuatrocaminos.webp"
+   },
+   {id: 11,
+    productName: "5555 Interstella - Daft Punk",
+    price: 355,
+    quanty: 1,
+    img: "./imagenes/3a98c01a45b740918b6e4b02f456ff2f.webp"
+   },
+   {id: 12,
+    productName: "Dark side in the moon - Pink Floyd",
+    price: 265,
+    quanty: 1,
+    img: "./imagenes/31ESVCFE0SL._AC_.jpg"
+   }
+]
